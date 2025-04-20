5f3da1fce2d325bae048596fdf4437d310c78886
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-04-20 13:43:34 +0530

    upadated MONGODB URL

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const quotes = [
 app.use('/habits', habitRoutes);  
 app.use('/auth', authRoutes);
 
-mongoose.connect(process.env.MONGoDB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
